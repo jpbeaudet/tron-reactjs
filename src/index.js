cfc591b248524b2d564bcb1524d813e2d69df82f
Jean-Philippe Beaudet <jpbeaudet@users.noreply.github.com>
2024-11-17 05:12:39 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+
+
+export { default as WalletSwitcher } from './components/WalletSwitcher';
 export { default as TronReactProvider } from './TronReactProvider';
 export { default as WalletProvider } from './components/WalletProvider';
 export { default as useWallet } from './hooks/useWallet';
 export { default as useContract } from './hooks/useContract';
 export { default as useEvents } from './hooks/useEvents';
+export { detectWallet } from './walletDetection';
+export { trackTransaction } from './transactionUtils';
+export { listenForEvents } from './smartContractUtils';
 export * as tronUtils from './utils/tronUtils';
