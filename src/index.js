3016e228ece2a3bde2de0847f39acddd7c97c5f5
Jean-Philippe Beaudet <jpbeaudet@users.noreply.github.com>
2024-11-17 01:25:03 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { default as TronReactProvider } from './TronReactProvider';
+export { default as WalletProvider } from './components/WalletProvider';
+export { default as useWallet } from './hooks/useWallet';
+export { default as useContract } from './hooks/useContract';
+export { default as useEvents } from './hooks/useEvents';
+export * as tronUtils from './utils/tronUtils';
